4d2c1a171fd7bd0e6df3f514a4b248e6022bee33
Max <maxcharrier359@gmail.com>
2021-09-30 09:47:42 +0200

    add ex8 & ex9

--- exercices/ex8.py
+++ exercices/ex8.py
@@ -1,14 +1,31 @@
 from random import randint
+import matplotlib.pyplot as plt
 
-puce = 0
+positionPuce = 0
 bande = 100
 sauts = 0
 listeSauts = []
 
-while puce < bande:
-  s = randint(1, 5)
-  puce += s
-  listeSauts.append(s)
+while positionPuce < bande:
+  saut = randint(1, 5)
+  positionPuce += saut
+  listeSauts.append(saut)
   sauts += 1
 
-print(f"Nombre de saut : {sauts} Listes sauts : {listeSauts} Minimum : {min(listeSauts)} Maximum : {max(listeSauts)} Moyenne : {sum(listeSauts) / len(listeSauts)}")
+print(f"Nombre de saut : {sauts} Listes sauts : {[]} Minimum : {min(listeSauts)} Maximum : {max(listeSauts)} Moyenne : {sum(listeSauts) / len(listeSauts)}")
+
+positions = 0
+listePositions = []
+
+for saut in listeSauts:
+  positions += saut
+  listePositions.append(positions)
+
+listeX = list(range(1, len(listePositions) + 1))
+listeY = listePositions
+
+plt.figure()
+plt.plot(listeX, listeY, label="Evolution de la puce")
+plt.legend()
+plt.grid(True)
+plt.show()
---
+++ exercices/ex9.py
@@ -0,0 +1,13 @@
+from pprint import pprint
+
+tab1 = []
+
+for table in range(11):
+    resultats = []
+
+    for i in range(11):
+        resultats.append(table * i)
+
+    tab1.append(resultats)
+
+pprint(tab1))
