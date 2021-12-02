0a541ef6b69cdad5de2a00fdb0d83a321881e4f0
Max Charrier <git@icecodder.me>
2021-12-02 12:38:55 +0100

    finish exercice 6

--- exercices/codage_python/ex6.py
+++ exercices/codage_python/ex6.py
@@ -4,8 +4,17 @@ table = prettytable.PrettyTable([" ", "0", "1", "2", "3", "4", "5", "6", "7", "8
 
 base_number = 32
 line_size = 16
-for i in range(2, 16):
-    print([hex(i).split("x")[1].upper(), [chr(int(hex(j), 0)) for j in range(base_number, base_number + line_size)]])
+for i in range(2, 8):
+    line = [chr(j) for j in range(base_number, base_number + line_size)]
+    line.insert(0, hex(i).split("x")[1].upper())
+    table.add_row(line)
     base_number += line_size
 
-#print(table)
+base_number = 128 + 2 * 16
+for i in range(10, 16):
+    line = [chr(j) for j in range(base_number, base_number + line_size)]
+    line.insert(0, hex(i).split("x")[1].upper())
+    table.add_row(line)
+    base_number += line_size
+
+print(table)
